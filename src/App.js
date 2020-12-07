148f50e7b436f53af63affe030c52a60e398cf5e
dizzyLee <dizzyLee@126.com>
2020-12-07 16:35:41 +0800

    add PriceList component and ionicon font

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
+/*
+ * @Author: LiYifan
+ * @Date: 2020-12-07 14:20:26
+ * @Description: 
+ */
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PriceList from './components/PriceList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+  {
+    "id": 1,
+    "title": "去秦皇岛",
+    "price": 200,
+    "date": "2018-09-10",
+    "category": {
+      "id": 1,
+      "name": "旅行",
+      "type": "outcome",
+      "iconName": "ios-plane"
+    }
+  },{
+    "id": 2,
+    "title": "打架斗殴",
+    "price": 1000,
+    "date": "2018-09-10",
+    "category": {
+      "id": 1,
+      "name": "旅行",
+      "type": "outcome",
+      "iconName": "ios-plane"
+    }
+  },
+]
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header> */}
+        <PriceList
+          items={items}
+          onModifyItem={(item) => {alert(item.id)}}
+          onDeleteItem={(item) => {alert('delete' + item.id)}}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/PriceList.js
@@ -0,0 +1,61 @@
+/*
+ * @Author: LiYifan
+ * @Date: 2020-12-07 15:05:31
+ * @Description: 
+ */
+import React from 'react';
+import Ionicon from 'react-ionicons';
+
+const PriceList = ({ items, onModifyItem, onDeleteItem }) => {
+  return (
+    <ul className="list-group list-group-flush">
+      {
+        items.map((item) => (
+          <li className="list-group-item d-flex justify-content-between align-items-center" key={item.id}>
+            <span className="col-1 badge badge-primary">
+              <Ionicon
+                className="rounded-circle"
+                fontSize="30px"
+                style={{ backgroundColor: '#007bff', padding: '5px' }}
+                color={'#fff'}
+                icon={item.category.iconName}
+              ></Ionicon>
+            </span>
+            <span className="col-5">{item.title}</span>
+            <span className="col-2 font-weight-bold">
+              {(item.category.type === 'income') ? '+' : '-'}
+              {item.price}元
+            </span>
+            <span className="col-2">{item.date}</span>
+            <a
+              className="col-1"
+              onClick={() => { onModifyItem(item) }}
+            >
+              <Ionicon
+                className="rounded-circle"
+                fontSize="30px"
+                style={{ backgroundColor: '#28a745', padding: '5px', cursor: 'pointer' }}
+                color={'#fff'}
+                icon='ios-create-outline'
+              ></Ionicon>
+            </a>
+            <a
+              className="col-1"
+              onClick={() => { onDeleteItem(item) }}
+            >
+              <Ionicon
+                className="rounded-circle"
+                fontSize="30px"
+                style={{ backgroundColor: '#dc3545', padding: '5px', cursor: 'pointer' }}
+                color={'#fff'}
+                icon='ios-close'
+              ></Ionicon>
+            </a>
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+export default PriceList;
